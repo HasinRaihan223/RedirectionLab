f804ce1d119fb3fb4e610c71c6d654e2f804a0e4
HasinRaihan223 <hraihan20@stuy.edu>
2021-01-03 13:13:42 -0500

    Implemented piglatin 2

--- PigLatin.java
+++ PigLatin.java
@@ -5,6 +5,16 @@ public class PigLatin {
     // System.out.println(pigLatinSimple("pie"));
     // System.out.println(pigLatinSimple("david"));
     // System.out.println(pigLatinSimple("aaron"));
+
+    // System.out.println(pigLatin("Block"));
+    // System.out.println(pigLatin("Nay"));
+    // System.out.println(pigLatin("Say"));
+    // System.out.println(pigLatin("iphone"));
+    // System.out.println(pigLatin("the"));
+    // System.out.println(pigLatin("check"));
+    // System.out.println(pigLatin("skee"));
+    // System.out.println(pigLatin("emu"));
+    // System.out.println(pigLatin("grade"));
   }
 
   public static String pigLatinSimple(String s){
@@ -24,4 +34,23 @@ public class PigLatin {
       return s.substring(1) + first + "ay";
     }
   }
+
+  public static String pigLatin(String s){
+    boolean hasDigraph = false;
+    s = s.toLowerCase();
+    String digraph = s.substring(0,2);
+    String[] digraphs = new String[] {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    for (int i = 0; i < digraphs.length; i++){
+      if (s.substring(0,2).equals(digraphs[i])){
+        hasDigraph = true;
+      }
+    }
+    if (hasDigraph==true){
+      return s.substring(2) + digraph + "ay";
+    }
+    else {
+      return pigLatinSimple(s);
+    }
+  }
+
 }
