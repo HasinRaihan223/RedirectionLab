0b1cddf6088889b23f729668b686bb41a95ea127
HasinRaihan223 <hraihan20@stuy.edu>
2021-01-03 12:36:31 -0500

    Implemented piglatin 1

--- PigLatin.java
+++ PigLatin.java
@@ -1,5 +1,27 @@
 public class PigLatin {
   public static void main(String[]args){
-    
+    //Test
+    // System.out.println(pigLatinSimple("mock"));
+    // System.out.println(pigLatinSimple("pie"));
+    // System.out.println(pigLatinSimple("david"));
+    // System.out.println(pigLatinSimple("aaron"));
+  }
+
+  public static String pigLatinSimple(String s){
+    boolean vowel = false;
+    s = s.toLowerCase();
+    char first = s.charAt(0);
+    if (s.substring(0,1).equals("a") || s.substring(0,1).equals("e") || s.substring(0,1).equals("i") || s.substring(0,1).equals("o") || s.substring(0,1).equals("u")){
+        vowel = true;
+    }
+    else if (!(s.substring(0,1).equals("a") || s.substring(0,1).equals("e") || s.substring(0,1).equals("i") || s.substring(0,1).equals("o") || s.substring(0,1).equals("u"))){
+        vowel = false;
+    }
+    if (vowel==true){
+      return s + "hay";
+    }
+    else {
+      return s.substring(1) + first + "ay";
+    }
   }
 }
